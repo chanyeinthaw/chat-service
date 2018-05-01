e0c71126622a6d087f98339e5a07a30c1d87947d
Chan Nyein Thaw <chanyeinthaw@gmail.com>
2018-05-01 13:02:31 +0630

    Added web-auth function

---
+++ src/web-auth.js
@@ -0,0 +1,15 @@
+const request = require('sync-request');
+const config = require('../config.js');
+
+const authURL = `http://${config.server.host}:${config.server.port}/api/io-auth`;
+const requestMethod = 'GET';
+
+module.exports = function(sessionId) {
+	let res = request(requestMethod, `${authURL}?session_id=${sessionId}`);
+
+	if (res.statusCode === 200) {
+		return JSON.parse(res.getBody());
+	}
+
+	return {success: false};
+};
