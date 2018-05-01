186dce3a8ee3762302dc48005815a2a05ab10f91
Chan Nyein Thaw <chanyeinthaw@gmail.com>
2018-05-01 15:31:27 +0630

    Added example config file

---
+++ config.example.js
@@ -0,0 +1,17 @@
+module.exports = {
+	server: {
+		host: 'localhost',
+		port: '8000'
+	},
+
+	ioConfig: {
+		accessKey: 'foolish'
+	},
+
+	mysql: {
+		host: 'localhost',
+		user: 'root',
+		password: '',
+		database: 'bizlaw'
+	}
+};
