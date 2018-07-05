79bce94fdbf0213adc76224a625be1eedc7d957a
Chan Nyein Thaw <chanyeinthaw@gmail.com>
2018-07-05 16:54:02 +0630

    Fix: confix.secure -> config.server.secure

--- src/HTTPServer.js
+++ src/HTTPServer.js
@@ -22,26 +22,26 @@ class HTTPServer {
         let unsecurePort = this._config.server.portUnSecure
         let host = this._config.server.host
         if (host) {
-            if (this._config.secure) {
+            if (this._config.server.secure) {
                 this._secureServer.listen(port, host, this._serverErrorHandler)
                 this._unSecureServer.listen(unsecurePort, host, this._serverErrorHandler)
             } else {
-                this._unSecureServer.listen(port, host, this._serverErrorHandler)
+                this._unSecureServer.listen(unsecurePort, host, this._serverErrorHandler)
             }
         } else {
-            if (this._config.secure) {
+            if (this._config.server.secure) {
                 this._secureServer.listen(port, this._serverErrorHandler)
                 this._unSecureServer.listen(unsecurePort, this._serverErrorHandler)
             } else {
-                this._unSecureServer.listen(port, this._serverErrorHandler)
+                this._unSecureServer.listen(unsecurePort, this._serverErrorHandler)
             }
         }
 
-        console.log(`HTTPS Server Started ${host ? host : ':'}:${port}`)
+        console.log(`HTTPS Server Started ${host ? host : ':'}:${this._config.server.secure ? port : unsecurePort}`)
     }
 
     get server() {
-        return this._config.secure ? this._secureServer : this._unSecureServer
+        return this._config.server.secure ? this._secureServer : this._unSecureServer
     }
 
     set serverErrorHandler (value) {
